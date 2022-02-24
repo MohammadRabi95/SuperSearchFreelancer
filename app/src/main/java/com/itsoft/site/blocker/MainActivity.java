6baa0b8e855c92ead21ed70550024ac2142b6318
asadmehar49 <asadmeharasadmehar786@gmail.com>
2022-02-24 18:54:24 +0500

    update

--- app/src/main/java/com/itsoft/site/blocker/MainActivity.java
+++ app/src/main/java/com/itsoft/site/blocker/MainActivity.java
@@ -2,18 +2,29 @@ package com.itsoft.site.blocker;
 
 import android.Manifest;
 import android.annotation.SuppressLint;
+import android.app.Activity;
+import android.content.ClipData;
+import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.content.res.Configuration;
 import android.graphics.Bitmap;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.FragmentManager;
 
+import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
+import android.os.Environment;
 import android.os.Handler;
 import android.os.Looper;
+import android.provider.MediaStore;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.util.Log;
 import android.view.View;
 import android.webkit.JavascriptInterface;
@@ -26,6 +37,7 @@ import android.webkit.WebSettings;
 import android.webkit.WebStorage;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
+import android.widget.EditText;
 import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
@@ -33,7 +45,12 @@ import android.widget.Toast;
 
 import com.google.android.material.switchmaterial.SwitchMaterial;
 
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Locale;
@@ -53,6 +70,13 @@ public class MainActivity extends AppCompatActivity implements VoiceRecognizerIn
     int pageLoaderCounter = 0;
     private String urlFinished = "";
     boolean doHide = true;
+    private EditText searchEditText;
+
+//    private static String file_type = "*/*";
+//    private String cam_file_data = null;
+//    private ValueCallback<Uri> file_data;
+//    private ValueCallback<Uri[]> file_path;
+//    private final static int file_req_code = 1;
 
     @SuppressLint({"SetJavaScriptEnabled", "AddJavascriptInterface"})
     @Override
@@ -71,6 +95,7 @@ public class MainActivity extends AppCompatActivity implements VoiceRecognizerIn
         adblockSwitch = findViewById(R.id.adblockSwitch);
         home = findViewById(R.id.home);
         microphone = findViewById(R.id.microphone);
+        searchEditText = findViewById(R.id.search);
 
         webView.getSettings().setJavaScriptEnabled(true);
         webView.addJavascriptInterface(this, "sb");
@@ -118,18 +143,153 @@ public class MainActivity extends AppCompatActivity implements VoiceRecognizerIn
         adblockSwitch.setOnCheckedChangeListener((buttonView, isChecked) -> {
 
             if (isChecked) {
+
+                isAdBlockingEnabled = "1";
+                hmm("ads on");
+            } else {
                 AdBlocker.init(this);
                 isAdBlockingEnabled = "0";
                 hmm("ads off");
-            } else {
-                isAdBlockingEnabled = "1";
-                hmm("ads on");
             }
         });
 
         microphone.setOnClickListener(v -> {
             requestRecordAudioPermission();
         });
+
+//        Search text
+        searchEditText.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @SuppressLint("JavascriptInterface")
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                String searchUrl = searchEditText.getText().toString().trim();
+                webView.loadUrl(GetSearchUrl(searchUrl));
+//                Toast.makeText(MainActivity.this, searchUrl, Toast.LENGTH_SHORT).show();
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
+
+//        webView.setWebChromeClient(new WebChromeClient() {
+//
+//            /*-- handling input[type="file"] requests for android API 21+ --*/
+//            public boolean onShowFileChooser(WebView webView, ValueCallback<Uri[]> filePathCallback, FileChooserParams fileChooserParams) {
+//
+//                if (file_permission() && Build.VERSION.SDK_INT >= 21) {
+//                    file_path = filePathCallback;
+//                    Intent takePictureIntent = null;
+//                    Intent takeVideoIntent = null;
+//
+//                    boolean includeVideo = false;
+//                    boolean includePhoto = false;
+//
+//                    /*-- checking the accept parameter to determine which intent(s) to include --*/
+//
+//                    paramCheck:
+//                    for (String acceptTypes : fileChooserParams.getAcceptTypes()) {
+//                        String[] splitTypes = acceptTypes.split(", ?+");
+//                        /*-- although it's an array, it still seems to be the whole value; split it out into chunks so that we can detect multiple values --*/
+//                        for (String acceptType : splitTypes) {
+//                            switch (acceptType) {
+//                                case "*/*":
+//                                    includePhoto = true;
+//                                    includeVideo = true;
+//                                    break paramCheck;
+//                                case "image/*":
+//                                    includePhoto = true;
+//                                    break;
+//                                case "video/*":
+//                                    includeVideo = true;
+//                                    break;
+//                            }
+//                        }
+//                    }
+//
+//                    if (fileChooserParams.getAcceptTypes().length == 0) {
+//
+//                        /*-- no `accept` parameter was specified, allow both photo and video --*/
+//
+//                        includePhoto = true;
+//                        includeVideo = true;
+//                    }
+//
+//                    if (includePhoto) {
+//                        takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+//                        if (takePictureIntent.resolveActivity(MainActivity.this.getPackageManager()) != null) {
+//                            File photoFile = null;
+//                            try {
+//                                photoFile = create_image();
+//                                takePictureIntent.putExtra("PhotoPath", cam_file_data);
+//                            } catch (IOException ex) {
+//                                Log.e("TAG", "Image file creation failed", ex);
+//                            }
+//                            if (photoFile != null) {
+//                                cam_file_data = "file:" + photoFile.getAbsolutePath();
+//                                takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, Uri.fromFile(photoFile));
+//                            } else {
+//                                cam_file_data = null;
+//                                takePictureIntent = null;
+//                            }
+//                        }
+//                    }
+//
+//                    if (includeVideo) {
+//                        takeVideoIntent = new Intent(MediaStore.ACTION_VIDEO_CAPTURE);
+//                        if (takeVideoIntent.resolveActivity(MainActivity.this.getPackageManager()) != null) {
+//                            File videoFile = null;
+//                            try {
+//                                videoFile = create_video();
+//                            } catch (IOException ex) {
+//                                Log.e("TAG", "Video file creation failed", ex);
+//                            }
+//                            if (videoFile != null) {
+//                                cam_file_data = "file:" + videoFile.getAbsolutePath();
+//                                takeVideoIntent.putExtra(MediaStore.EXTRA_OUTPUT, Uri.fromFile(videoFile));
+//                            } else {
+//                                cam_file_data = null;
+//                                takeVideoIntent = null;
+//                            }
+//                        }
+//                    }
+//
+//                    Intent contentSelectionIntent = new Intent(Intent.ACTION_GET_CONTENT);
+//                    contentSelectionIntent.addCategory(Intent.CATEGORY_OPENABLE);
+//                    contentSelectionIntent.setType(file_type);
+//
+//
+//                    Intent[] intentArray;
+//                    if (takePictureIntent != null && takeVideoIntent != null) {
+//                        intentArray = new Intent[]{takePictureIntent, takeVideoIntent};
+//                    } else if (takePictureIntent != null) {
+//                        intentArray = new Intent[]{takePictureIntent};
+//                    } else if (takeVideoIntent != null) {
+//                        intentArray = new Intent[]{takeVideoIntent};
+//                    } else {
+//                        intentArray = new Intent[0];
+//                    }
+//
+//                    Intent chooserIntent = new Intent(Intent.ACTION_CHOOSER);
+//                    chooserIntent.putExtra(Intent.EXTRA_INTENT, contentSelectionIntent);
+//                    chooserIntent.putExtra(Intent.EXTRA_TITLE, "File chooser");
+//                    chooserIntent.putExtra(Intent.EXTRA_INITIAL_INTENTS, intentArray);
+//                    startActivityForResult(chooserIntent, file_req_code);
+//                    return true;
+//                } else {
+//                    return false;
+//                }
+//            }
+//
+//
+//        });
 
     }
 
@@ -160,7 +320,7 @@ public class MainActivity extends AppCompatActivity implements VoiceRecognizerIn
         public WebResourceResponse shouldInterceptRequest(WebView view, String url) {
             boolean ad;
             Log.e("shouldInterceptRequest:", " Called");
-            if (adblockSwitch.isChecked()) {
+            if (!adblockSwitch.isChecked()) {
                 if (!loadedUrls.containsKey(url)) {
                     ad = AdBlocker.isAd(url);
                     loadedUrls.put(url, ad);
@@ -448,4 +608,100 @@ public class MainActivity extends AppCompatActivity implements VoiceRecognizerIn
         Toast.makeText(this, spokenText, Toast.LENGTH_SHORT).show();
         webView.loadUrl(GetSearchUrl(spokenText));
     }
+
+//    @Override
+//    protected void onActivityResult(int requestCode, int resultCode, Intent intent) {
+//        super.onActivityResult(requestCode, resultCode, intent);
+//        if (Build.VERSION.SDK_INT >= 21) {
+//            Uri[] results = null;
+//
+//            /*-- if file request cancelled; exited camera. we need to send null value to make future attempts workable --*/
+//            if (resultCode == Activity.RESULT_CANCELED) {
+//                file_path.onReceiveValue(null);
+//                return;
+//            }
+//
+//            /*-- continue if response is positive --*/
+//            if (resultCode == Activity.RESULT_OK) {
+//                if (null == file_path) {
+//                    return;
+//                }
+//                ClipData clipData;
+//                String stringData;
+//
+//                try {
+//                    clipData = intent.getClipData();
+//                    stringData = intent.getDataString();
+//                } catch (Exception e) {
+//                    clipData = null;
+//                    stringData = null;
+//                }
+//                if (clipData == null && stringData == null && cam_file_data != null) {
+//                    results = new Uri[]{Uri.parse(cam_file_data)};
+//                } else {
+//                    if (clipData != null) {
+//                        final int numSelectedFiles = clipData.getItemCount();
+//                        results = new Uri[numSelectedFiles];
+//                        for (int i = 0; i < clipData.getItemCount(); i++) {
+//                            results[i] = clipData.getItemAt(i).getUri();
+//                        }
+//                    } else {
+//                        try {
+//                            Bitmap cam_photo = (Bitmap) intent.getExtras().get("data");
+//                            ByteArrayOutputStream bytes = new ByteArrayOutputStream();
+//                            cam_photo.compress(Bitmap.CompressFormat.JPEG, 100, bytes);
+//                            stringData = MediaStore.Images.Media.insertImage(this.getContentResolver(), cam_photo, null, null);
+//                        } catch (Exception ignored) {
+//                        }
+//
+//                        results = new Uri[]{Uri.parse(stringData)};
+//                    }
+//                }
+//            }
+//
+//            file_path.onReceiveValue(results);
+//            file_path = null;
+//        } else {
+//            if (requestCode == file_req_code) {
+//                if (null == file_data) return;
+//                Uri result = intent == null || resultCode != RESULT_OK ? null : intent.getData();
+//                file_data.onReceiveValue(result);
+//                file_data = null;
+//            }
+//        }
+//    }
+//
+//    @Override
+//    public void onConfigurationChanged(Configuration newConfig) {
+//        super.onConfigurationChanged(newConfig);
+//    }
+//
+//    public boolean file_permission() {
+//        if (Build.VERSION.SDK_INT >= 23 && (ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED ||
+//                ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) ) {
+//
+//            ActivityCompat.requestPermissions(MainActivity.this, new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE,
+//                    Manifest.permission.CAMERA}, 1);
+//            return false;
+//        } else {
+//            return true;
+//        }
+//    }
+//
+//    private File create_image() throws IOException {
+//        @SuppressLint("SimpleDateFormat") String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+//        String imageFileName = "img_" + timeStamp + "_";
+//        File storageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+//        return File.createTempFile(imageFileName, ".jpg", storageDir);
+//    }
+//
+//    private File create_video() throws IOException {
+//        @SuppressLint("SimpleDateFormat")
+//        String file_name = new SimpleDateFormat("yyyy_mm_ss").format(new Date());
+//        String new_name = "file_" + file_name + "_";
+//        File sd_directory = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+//        return File.createTempFile(new_name, ".3gp", sd_directory);
+//    }
+
+
 }
